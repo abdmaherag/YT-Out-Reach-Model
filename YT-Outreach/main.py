da456b6a4746d39290409a59a5d54d25d558709c
Abdulrahman <abdulrahmanmaherag@gmail.com>
2025-07-09 21:07:08 +0300

    Added project steps to readme file
    Added try block to check link validation
    Added option to fetch by @username

--- YT-Outreach/main.py
+++ YT-Outreach/main.py
@@ -1,11 +1,25 @@
 import requests
 
-ytlink = input("Please Insert Youtuber Link:") #might add option @ to get youtuber
+ytlink = input("Please Insert Youtuber Link or @username:") #added option to lookup youtuber by @username
 
-response = requests.get(ytlink)
+#look up youtuber by @username
+if not ytlink.startswith ("http"):
+    ytlink = f"https://www.youtube.com/@{ytlink}/videos"
+
 
 #Checking if link is valid or not
-if response.status_code == 404:
-    print("Page not found (404)")
-elif response.status_code == 200:
-    print("Page loaded successfully!")
+try:
+    response = requests.get(ytlink)
+
+    if response.status_code == 404:
+        print("Invalid Youtuber")
+    elif response.status_code == 200:
+        print("Valid Youtuber")
+
+except requests.exceptions.MissingSchema:
+    print("Invalid URL (Missing schema like https://).")
+except requests.exceptions.RequestException as e:
+    print(f" Network or URL error: {e}")
+
+
+#STEP 2
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Steps:
+1.User inserts youtuber link or @
+2.Check last 3 recent videos, if the video is older than 2 months, it is dismissed
+3.Get scripts of the videos and save each one individually
+4.analyze the script
+5.Using the analyzed script to create a personalized email
+6.Send email and 3 follow ups
